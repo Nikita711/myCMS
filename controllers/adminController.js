d8c8ffe2eca5759622ce5f5881adaf4147eed316
Nikita711 <abhi9041355@gmail.com>
2020-10-26 19:14:30 +0530

    Admin page ready

---
+++ controllers/adminController.js
@@ -0,0 +1,17 @@
+module.exports = {
+  index: (req, res) => {
+    res.render("admin/index");
+  },
+
+  getPosts: (req, res) => {
+    res.send("All Posts");
+  },
+
+  submitPosts: (req, res) => {
+    res.send("Post Submitted");
+  },
+
+  createPosts: (req, res) => {
+    res.render("admin/posts/create");
+  },
+};
